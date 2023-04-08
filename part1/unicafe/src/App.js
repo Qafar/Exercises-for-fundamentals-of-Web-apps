a15a90515c9fbf809fb1466229ef3e69b461bfc8
Gafar Akhmedov <qafar313@gmail.com>
2023-04-08 23:37:59 +0400

    Created new counter app named as unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) =><button onClick={handleClick}>{text}</button>
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value ', counter)
+  const increaseByOne = () => {
+    console.log('increasing, value before ', counter)
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing, value before ', counter)
+    setCounter(counter - 1)
+  }
+  const setToZero = () => {
+    console.log('resetting value to zero, value before ',counter)
+    setCounter(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Display counter={counter} />
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
     </div>
-  );
+  )
 }
 
 export default App;
