85f0c60a17383e4341e468281d94b2eb32d70944
Gafar Akhmedov <qafar313@gmail.com>
2023-04-10 00:56:35 +0400

    Completed unicafe app

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,30 +1,61 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
 
-const Display = props => <div>{props.value}</div>
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+
+  if (all === 0) {
+    return <p>No feedback given</p>;
+  }
+
+  const average = (good - bad) / all;
+  const positiveByPercent = `${(good / all) * 100}%`;
+
+  return (
+    <table border="1">
+      <tbody>
+        <tr>
+          <th>Statistic</th>
+          <th>Value</th>
+        </tr>
+        <StatisticLine text='good' value={good} />
+        <StatisticLine text='neutral' value={neutral} />
+        <StatisticLine text='bad' value={bad} />
+        <StatisticLine text='all' value={all} />
+        <StatisticLine text='average' value={average.toFixed(1)} />
+        <StatisticLine text='positive' value={positiveByPercent} />
+      </tbody>
+    </table>
+  );
+};
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
-  const [value, setValue] = useState(10)
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  const setToValue = (newValue) => {
-    console.log('value now ', newValue)
-    setValue(newValue)
-  }
-  
+  const handleFeedback = (type) => {
+    setFeedback((prev) => ({ ...prev, [type]: prev[type] + 1 }));
+  };
 
   return (
     <div>
-      <Display value={value} /> 
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <h2>give feedback</h2>
+      <Button handleClick={() => handleFeedback('good')} text='good' />
+      <Button handleClick={() => handleFeedback('neutral')} text='neutral' />
+      <Button handleClick={() => handleFeedback('bad')} text='bad' />
+      <h2>statistics</h2>
+      <Statistics {...feedback} />
     </div>
-  )
-}
+  );
+};
 
 export default App;
